b23f388f049627d91b2bf2e7b0b22a2aa1387a13
Arnold Vink <info@arnoldvink.com>
2026-03-21 23:26:26 +0100

    - Changed CComHeapPtr to AVFin.

--- ScreenCapture/CaptureCodecCheck.cpp
+++ ScreenCapture/CaptureCodecCheck.cpp
@@ -7,11 +7,21 @@ namespace
 	{
 		try
 		{
+			//Get video encoders
 			UINT32 numMFTActivate = 0;
-			CComHeapPtr<IMFActivate*> pppMFTActivate;
+			auto pppMFTActivate = AVFin<IMFActivate**>(AVFinMethod::Custom);
+			pppMFTActivate.SetReleaser([&](auto releasePointer)
+				{
+					for (int i = 0; i < numMFTActivate; i++)
+					{
+						releasePointer[i]->Release();
+					}
+					CoTaskMemFree(releasePointer);
+				});
 			MFT_REGISTER_TYPE_INFO mftRegisterTypeInfo = { MFMediaType_Video, subType };
-			hResult = MFTEnumEx(MFT_CATEGORY_VIDEO_ENCODER, MFT_ENUM_FLAG_HARDWARE, NULL, &mftRegisterTypeInfo, &pppMFTActivate, &numMFTActivate);
-			pppMFTActivate.Free();
+			hResult = MFTEnumEx(MFT_CATEGORY_VIDEO_ENCODER, MFT_ENUM_FLAG_HARDWARE, NULL, &mftRegisterTypeInfo, &pppMFTActivate.Get(), &numMFTActivate);
+
+			//Check result
 			if (SUCCEEDED(hResult) && numMFTActivate > 0)
 			{
 				return true;
